c32b9fe32a90c4468f85e543697ff6469a09dc9c
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-01-31 01:21:27 -0800

    Get started on joystick interaction/rotation

---
+++ Rogue Starfighter VR V1/Assets/LocalRotateTowardsSlerp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalRotateTowardsSlerp : MonoBehaviour
+{
+    [Range(0,1)]
+    public float slerpFactor = 1;
+
+    [HideInInspector]
+    public Quaternion TargetRotation;
+
+    void FixedUpdate()
+    {
+        if(transform.localRotation != TargetRotation)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, TargetRotation, slerpFactor);
+        }
+    }
+}
---
+++ Rogue Starfighter VR V1/Assets/TripleRotationalDrive.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class TripleRotationalDrive : MonoBehaviour
+{
+    private Interactable interactable;
+
+    public Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.DetachFromOtherHand;
+
+    private Quaternion initialHandRotationOnGrab;  // store initial rotation to use as difference // TODO: implement
+    private LocalRotateTowardsSlerp LocalRotateTowardsSlerp;
+
+    private void Start()
+    {
+        interactable = GetComponent<Interactable>();
+        LocalRotateTowardsSlerp = GetComponent<LocalRotateTowardsSlerp>();
+    }
+    protected virtual void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+
+    protected virtual void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+    protected virtual void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            // was just grabbed
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+        }
+    }
+
+    protected virtual void HandAttachedUpdate(Hand hand)
+    {
+        //UpdateLinearMapping(hand.transform);
+
+        if (hand.IsGrabEnding(this.gameObject))
+        {
+            // just let go
+            hand.DetachObject(gameObject);
+            LocalRotateTowardsSlerp.TargetRotation = Quaternion.identity;
+        }
+        else
+        {
+            // still holding on
+            // set target rotation
+            // TODO: set based on difference from initial grab rotation
+            // TODO: incorporate RestrictRotation inline here instead
+            // TODO: scale rotation
+            LocalRotateTowardsSlerp.TargetRotation = hand.transform.localRotation;
+        }
+    }
+}
