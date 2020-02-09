d12972b7378d4f985979aa4bb6c0ce57b2f3f57b
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-08 23:47:06 -0800

    Build most of ISD spawning system

---
+++ Rogue Starfighter VR V1/Assets/ISDSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ISDSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject imperialStarDestroyerPrefab;
+
+    public void SpawnISD(Vector3 spawnLocation, Vector3 positionToPointTo)
+    {
+        GameObject ISD = Instantiate(imperialStarDestroyerPrefab, spawnLocation, Quaternion.identity, transform);  // parent to me
+        ISD.transform.LookAt(positionToPointTo);
+    }
+}
---
+++ Rogue Starfighter VR V1/Assets/TriggerISDSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerISDSpawn : MonoBehaviour
+{
+    [SerializeField] private ISDSpawner iSDSpawner;
+    [Tooltip("Will spawn ISD in at player's transform with a given position offset.")]
+    [SerializeField] private Transform playerShip;
+    [SerializeField] private Vector3 spawnPosOffset;
+    [SerializeField] private Vector3 spawnRotOffsetEuler;
+
+    public void SpawnISD()
+    {
+        Debug.Log("Spawning an ISD");
+        iSDSpawner.SpawnISD(playerShip.position + spawnPosOffset, Quaternion.Euler(spawnPosOffset) * playerShip.localRotation);
+    } 
+}
