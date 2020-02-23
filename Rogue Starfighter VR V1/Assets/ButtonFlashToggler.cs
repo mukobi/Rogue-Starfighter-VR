b72335433cb473d11b8cb465282811fe8fde168f
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-23 03:49:36 -0800

    make button flashing effect and color buttons r,g, white

---
+++ Rogue Starfighter VR V1/Assets/ButtonFlashToggler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFlashToggler : MonoBehaviour
+{
+    private Material material;
+
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    public void ToggleFlashing(bool toFlash)
+    {
+        material.SetFloat("__enable_flashing", toFlash ? 1.0f : 0.0f);
+    }
+
+    [ContextMenu("StartFlash")]
+    private void StartFlashing()
+    {
+        ToggleFlashing(true);
+    }
+
+    [ContextMenu("StopFlash")]
+    private void StopFlashing()
+    {
+        ToggleFlashing(false);
+    }
+}
