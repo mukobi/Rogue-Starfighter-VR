08f952ce3ce6de49e2e3a839fc6ba142b2c40dc5
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-23 13:56:41 -0800

    add in keyboard hacks

---
+++ Rogue Starfighter VR V1/Assets/KeyboardInputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyboardInputManager : MonoBehaviour
+{
+    public UnityEvent OnRKeyPressed;
+    public UnityEvent OnSKeyPressed;
+    public UnityEvent OnDKeyPressed;
+    public UnityEvent OnLKeyPressed;
+    public UnityEvent OnMKeyPressed;
+    public UnityEvent OnAKeyPressed;
+    public UnityEvent OnFKeyPressed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            OnRKeyPressed.Invoke();
+        if (Input.GetKeyDown(KeyCode.S))
+            OnSKeyPressed.Invoke();
+        if (Input.GetKeyDown(KeyCode.D))
+            OnDKeyPressed.Invoke();
+        if (Input.GetKeyDown(KeyCode.L))
+            OnLKeyPressed.Invoke();
+        if (Input.GetKeyDown(KeyCode.M))
+            OnMKeyPressed.Invoke();
+        if (Input.GetKeyDown(KeyCode.A))
+            OnAKeyPressed.Invoke();
+        if (Input.GetKeyDown(KeyCode.F))
+            OnFKeyPressed.Invoke();
+    }
+}
