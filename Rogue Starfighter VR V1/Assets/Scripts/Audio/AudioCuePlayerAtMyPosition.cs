114c03eda40963208b86e6491b92474d62ba2cba
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-23 07:38:59 -0800

    Make audio work, now it's a game

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Audio/AudioCuePlayerAtMyPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCuePlayerAtMyPosition : MonoBehaviour
+{
+    [SerializeField] private AudioCueScriptableObject audioCue = default;
+
+    public void PlayAtMyPosition()
+    {
+        audioCue.Play(transform.position);
+    }
+}
