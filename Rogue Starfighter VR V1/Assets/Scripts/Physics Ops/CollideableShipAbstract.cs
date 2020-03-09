9aeda7b6d43e9f0f85bf2781e564f01602e25e80
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-08 23:33:59 -0700

    Start detecting ship collisions (involved setting ContactPairsMode to Kinematic-Kinematic)

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Physics Ops/CollideableShipAbstract.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CollideableShipAbstract : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"{gameObject.name} collided with {collision.gameObject.name}");
+        HandleCollision(collision);
+    }
+
+    protected virtual void HandleCollision(Collision collision)
+    {
+        UnityEditor.EditorApplication.Beep();
+        if (collision.gameObject.GetComponent<CollideableShipAbstract>() != null)
+        {
+            // collided with another ship
+            Debug.Log($"Ship collision! {collision.contactCount} contacts");
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                Debug.DrawRay(contact.point, contact.normal * 10, Color.green, 1.0f);
+            }
+            //UnityEditor.EditorApplication.isPaused = true;
+        }
+    }
+}
