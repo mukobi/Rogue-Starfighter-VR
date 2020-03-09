83013787bf6f1861f940fd257a1f2c41993e0328
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-09 00:42:57 -0700

    Successfully fix ship collision normal calculation using raycast

--- Rogue Starfighter VR V1/Assets/Scripts/Physics Ops/CollideableShipBounceOff.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Physics Ops/CollideableShipBounceOff.cs
@@ -18,14 +18,60 @@ public class CollideableShipBounceOff : CollideableShipAbstract
             UnityEditor.EditorApplication.Beep();
             foreach (ContactPoint contact in collision.contacts)
             {
-                Debug.DrawRay(contact.point, contact.normal * 10, Color.green, 0.66f);
+                Debug.DrawRay(contact.point, contact.normal * 5, Color.blue, 0.66f);
             }
-            //UnityEditor.EditorApplication.isPaused = true;
 
             // assume first contact best (not sure if this works? ¯\_(ツ)_/¯)
             ContactPoint contactPoint = collision.GetContact(0);
-            BounceMyselfOffCollision(contactPoint.normal);
+            Vector3 actualCollidedSurfaceNormal = CalculateCollisionNormal(contactPoint);
+            Debug.DrawRay(contactPoint.point, actualCollidedSurfaceNormal * 16, Color.green, 0.66f);
+            BounceMyselfOffCollision(actualCollidedSurfaceNormal);
         }
+    }
+
+    private Vector3 CalculateCollisionNormal(ContactPoint contactPoint)
+    {
+        const float rayStepBackLength = 5.0f;
+        const float rayLength = 16.0f;
+
+
+        Vector3 point = contactPoint.point;
+        Vector3 contactNormal = contactPoint.normal;
+
+        // step back a bit
+        point += rayStepBackLength * contactNormal;
+
+        // cast a ray several times as far as your step back. This seems to work in most
+        // situations, at least when speeds are not ridiculously big
+        RaycastHit hitInfo;
+        Ray ray = new Ray(point, -contactNormal);
+        if (contactPoint.otherCollider.Raycast(ray, out hitInfo, rayLength))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.yellow, 0.66f);
+            UnityEditor.EditorApplication.isPaused = true;
+            // this is the actual collider surface normal
+            return hitInfo.normal;
+        }
+
+        // simple raycast in direction of contactNormal failed (maybe high incident angle, so 
+        // instead we need to raycast from the direction of this ship towards the collision point
+        Debug.LogWarning("No raycast hit from collision normal, trying raycast from ship");
+        Vector3 rayOrigin = transform.position - rayStepBackLength * transform.forward;
+        Vector3 rayDirection = (contactPoint.point - transform.position).normalized;
+        ray = new Ray(rayOrigin, rayDirection);
+        if (contactPoint.otherCollider.Raycast(ray, out hitInfo, rayLength))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.magenta, 0.66f);
+            UnityEditor.EditorApplication.isPaused = true;
+            // this is the actual collider surface normal
+            return hitInfo.normal;
+        }
+
+        // nothing worked!
+        Debug.LogError("No Raycast hit for calculating ship collision normal.");
+        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red, 0.66f);
+        UnityEditor.EditorApplication.isPaused = true;
+        return contactNormal;
     }
 
     private void BounceMyselfOffCollision(Vector3 normal)
