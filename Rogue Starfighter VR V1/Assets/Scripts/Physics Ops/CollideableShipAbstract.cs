5269bcc993f9c941a446ff5e881362e4578e24c3
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-09 20:36:13 -0700

    Enable toggling debug logging of ship collision

--- Rogue Starfighter VR V1/Assets/Scripts/Physics Ops/CollideableShipAbstract.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Physics Ops/CollideableShipAbstract.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public abstract class CollideableShipAbstract : MonoBehaviour
 {
+    protected readonly bool debugMode = false;
+
     private bool hasCollisionEnteredThisFrame;
+
+    UnityEventVector3 onShipCollision;
 
     private void LateUpdate()
     {
@@ -17,13 +21,16 @@ public abstract class CollideableShipAbstract : MonoBehaviour
     {
         if (!hasCollisionEnteredThisFrame)
         {
-            hasCollisionEnteredThisFrame = true;
-            Debug.Log($"{gameObject.name} collided with {collision.gameObject.name}");
-            HandleCollision(collision);
+            if (collision.gameObject.GetComponent<CollideableShipAbstract>() != null)
+            {
+                hasCollisionEnteredThisFrame = true;
+                if(debugMode) Debug.Log($"{gameObject.name} collided with {collision.gameObject.name}");
+                HandleShipCollision(collision);
+            }
         }
     }
 
-    protected virtual void HandleCollision(Collision collision)
+    protected virtual void HandleShipCollision(Collision collision)
     {
         // do nothing for now, see override in CollideableShipBounceOff.cs
     }
--- Rogue Starfighter VR V1/Assets/Scripts/Physics Ops/CollideableShipBounceOff.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Physics Ops/CollideableShipBounceOff.cs
@@ -11,25 +11,25 @@ public class CollideableShipBounceOff : CollideableShipAbstract
     [Tooltip("How far to jump in reflected direction on impact.")]
     [SerializeField] private float collisionJumpBias = default;
 
-    protected override void HandleCollision(Collision collision)
+    protected override void HandleShipCollision(Collision collision)
     {
-        if (collision.gameObject.GetComponent<CollideableShipAbstract>() != null)
+        // collided with another ship
+        if (debugMode)
         {
-            // collided with another ship
             Debug.Log($"Ship collision! {collision.contactCount} contacts");
             UnityEditor.EditorApplication.Beep();
             foreach (ContactPoint contact in collision.contacts)
             {
                 Debug.DrawRay(contact.point, contact.normal * 5, Color.blue, 0.66f);
             }
-
-            // assume first contact best (not sure if this works? ¯\_(ツ)_/¯)
-            ContactPoint contactPoint = collision.GetContact(0);
-            Vector3 actualCollidedSurfaceNormal = CalculateCollisionNormal(contactPoint);
-            Debug.DrawRay(contactPoint.point, actualCollidedSurfaceNormal * 16, Color.green, 0.66f);
-            BounceMyselfOffCollision(actualCollidedSurfaceNormal);
-            //UnityEditor.EditorApplication.isPaused = true;
         }
+
+        // assume first contact best (not sure if this works? ¯\_(ツ)_/¯)
+        ContactPoint contactPoint = collision.GetContact(0);
+        Vector3 actualCollidedSurfaceNormal = CalculateCollisionNormal(contactPoint);
+        if (debugMode) Debug.DrawRay(contactPoint.point, actualCollidedSurfaceNormal * 16, Color.green, 0.66f);
+        BounceMyselfOffCollision(actualCollidedSurfaceNormal);
+        //UnityEditor.EditorApplication.isPaused = true;
     }
 
     private Vector3 CalculateCollisionNormal(ContactPoint contactPoint)
@@ -50,28 +50,31 @@ public class CollideableShipBounceOff : CollideableShipAbstract
         Ray ray = new Ray(point, -contactNormal);
         if (contactPoint.otherCollider.Raycast(ray, out hitInfo, rayLength))
         {
-            Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.yellow, 0.66f);
+            if (debugMode) Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.yellow, 0.66f);
             // this is the actual collider surface normal
             return hitInfo.normal;
         }
 
         // simple raycast in direction of contactNormal failed (maybe high incident angle, so 
         // instead we need to raycast from the direction of this ship towards the collision point
-        Debug.LogWarning("No raycast hit from collision normal, trying raycast from ship");
+        if (debugMode) Debug.LogWarning("No raycast hit from collision normal, trying raycast from ship");
         Vector3 rayOrigin = transform.position - rayStepBackLength * transform.forward;
         Vector3 rayDirection = (contactPoint.point - transform.position).normalized;
         ray = new Ray(rayOrigin, rayDirection);
         if (contactPoint.otherCollider.Raycast(ray, out hitInfo, rayLength))
         {
-            Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.magenta, 0.66f);
+            if (debugMode) Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.magenta, 0.66f);
             // this is the actual collider surface normal
             return hitInfo.normal;
         }
 
         // nothing worked!
-        Debug.LogError("No Raycast hit for calculating ship collision normal.");
-        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red, 0.66f);
-        UnityEditor.EditorApplication.isPaused = true;
+        if (debugMode)
+        {
+            Debug.LogError("No Raycast hit for calculating ship collision normal.");
+            Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red, 0.66f);
+            UnityEditor.EditorApplication.isPaused = true;
+        }
         return contactNormal;
     }
 
@@ -79,7 +82,7 @@ public class CollideableShipBounceOff : CollideableShipAbstract
     {
         // calculate reflection of vector across normal: r = d - 2(dot(d,n))n
         Vector3 newForward = transform.forward - 2 * Vector3.Dot(transform.forward, normal) * normal;
-        Debug.Log(newForward);
+        if (debugMode) Debug.Log(newForward);
 
         // use same up vector as before to preserve rotation about forward axis
         Vector3 oldUp = transform.up;
