6a363394e126cde13eb7dccd10c442fbd3845f37
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-12 13:46:23 -0700

    Refactor delayed destruction away from prefab instantiator

--- Rogue Starfighter VR V1/Assets/Scripts/GameObject Ops/PrefabInstantiator.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/GameObject Ops/PrefabInstantiator.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class PrefabInstantiator : MonoBehaviour
 {
     public GameObject prefab;
-    public float destroyTime;
 
     [ContextMenu("InstantiatePrefabAtMyTransform")]
     public void InstantiatePrefabAtMyTransform()
     {
         GameObject gameObject = Instantiate(prefab, transform.position, transform.rotation);
-        Destroy(gameObject, destroyTime);
     }
 }
