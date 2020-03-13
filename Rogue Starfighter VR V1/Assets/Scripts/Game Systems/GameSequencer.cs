f0a1db30edc694c5cf160cbd47ebfefba2a1ef20
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-12 19:21:50 -0700

    set up game sequence management

--- Rogue Starfighter VR V1/Assets/Scripts/Game Systems/GameSequencer.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Game Systems/GameSequencer.cs
@@ -1,7 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Threading.Tasks;
 
 public class GameSequencer : MonoBehaviour
 {
+    async void Start()
+    {
+        await MainGameTask();
+    }
+
+    private async Task MainGameTask()
+    {
+        Debug.Log("Start main game sequence");
+
+        await Task.Delay(5000);
+
+        Debug.Log("End main game sequence");
+    }
 }
