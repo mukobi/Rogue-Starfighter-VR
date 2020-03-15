df626daaefc5a860f4cfbc8abef46b318c4c6d93
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-15 15:02:26 -0700

    Add AimTowardsPlayerBehaviour to boid composite behaviour

--- Rogue Starfighter VR V1/Assets/Scripts/Boids/Behaviour Scripts/AimTowardsPlayerBehaviour.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Boids/Behaviour Scripts/AimTowardsPlayerBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Flock/Behavior/AimTowardsPlayer")]
 public class AimTowardsPlayerBehaviour : BoidBehaviour
 {
     public override Vector3 CalculateDesiredForward(BoidAgent agent, List<Transform> context, BoidFlock flock)
