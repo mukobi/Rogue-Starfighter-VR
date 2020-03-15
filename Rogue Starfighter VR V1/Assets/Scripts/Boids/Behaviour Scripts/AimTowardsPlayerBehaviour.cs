42167b0bdf8bb2bc643aea7a01047fb5bd0ae9b2
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-15 15:21:48 -0700

    Make TIEs only aim at player when at least a distance away (let avoidance reduce collisions)

--- Rogue Starfighter VR V1/Assets/Scripts/Boids/Behaviour Scripts/AimTowardsPlayerBehaviour.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Boids/Behaviour Scripts/AimTowardsPlayerBehaviour.cs
@@ -7,10 +7,22 @@ public class AimTowardsPlayerBehaviour : BoidBehaviour
 {
     [Tooltip("Won't aim at player when angle to player is outside this.")]
     public float maxAngleToAim;
+
+    public float minDistToAim;
+    private float squareMinDistToAim;
+
+    [RuntimeInitializeOnLoadMethod]
+    private void RuntimeInitializeOnLoad()
+    {
+        squareMinDistToAim = minDistToAim * minDistToAim;
+    }
     
     public override Vector3 CalculateDesiredForward(BoidAgent agent, List<Transform> context, BoidFlock flock)
     {
-        Vector3 dirToPlayerNorm = (PlayerGlobalReference.I.shipRotationRoot.position - agent.transform.position).normalized;
+        Vector3 dirToPlayer = PlayerGlobalReference.I.shipRotationRoot.position - agent.transform.position;
+        if (dirToPlayer.sqrMagnitude < squareMinDistToAim)
+            return Vector3.zero;
+        Vector3 dirToPlayerNorm = dirToPlayer.normalized;
         if (Vector3.Angle(agent.transform.forward, dirToPlayerNorm) > maxAngleToAim)
             return Vector3.zero;
         return dirToPlayerNorm;
