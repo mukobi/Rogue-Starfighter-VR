5e3a59109c6077d3328ba1e2dbe3734741b1774a
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-15 15:11:02 -0700

    Make TIEs aim towards player strongly only when generally aiming at player

--- Rogue Starfighter VR V1/Assets/Scripts/Boids/Behaviour Scripts/AimTowardsPlayerBehaviour.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Boids/Behaviour Scripts/AimTowardsPlayerBehaviour.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Flock/Behavior/AimTowardsPlayer")]
 public class AimTowardsPlayerBehaviour : BoidBehaviour
 {
+    [Tooltip("Won't aim at player when angle to player is outside this.")]
+    public float maxAngleToAim;
+    
     public override Vector3 CalculateDesiredForward(BoidAgent agent, List<Transform> context, BoidFlock flock)
     {
-        return (PlayerGlobalReference.I.shipRotationRoot.position - agent.transform.position).normalized;
+        Vector3 dirToPlayerNorm = (PlayerGlobalReference.I.shipRotationRoot.position - agent.transform.position).normalized;
+        if (Vector3.Angle(agent.transform.forward, dirToPlayerNorm) > maxAngleToAim)
+            return Vector3.zero;
+        return dirToPlayerNorm;
     }
 }
