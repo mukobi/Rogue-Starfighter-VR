5574c73b024066dde8a2d7805ba7b9d81857bc83
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-15 14:50:18 -0700

    Write AimTowardsPlayerBehaviour

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Boids/Behaviour Scripts/AimTowardsPlayerBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTowardsPlayerBehaviour : BoidBehaviour
+{
+    private GameObject player;
+
+    public override Vector3 CalculateDesiredForward(BoidAgent agent, List<Transform> context, BoidFlock flock)
+    {
+        return (player.transform.position - agent.transform.position).normalized;
+    }
+}
