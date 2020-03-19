3fe726df887db7d6a2323464d95680c99a7434b2
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-18 20:21:02 -0700

    Add shield heath bar with custom shader and sprite

---
+++ Rogue Starfighter VR V1/Assets/Scripts/UI/LinearProgressBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearProgressBar : MonoBehaviour
+{
+    public float MinValue = 0;
+    public float MaxValue = 100;
+    [SerializeField] private float defaultValue = 100;
+
+    [SerializeField] private RectTransform fill;
+
+    private void Start()
+    {
+        SetValue(defaultValue);
+    }
+
+    public void SetValue(float value)
+    {
+        float x = (value - MinValue) / (MaxValue - MinValue);
+        fill.localScale = new Vector3(x, 1, 1);
+    }
+}
