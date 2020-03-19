84f5a205f34f827dc1843c6714a5ac66953af389
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-18 20:58:02 -0700

    Add shield font label and control via script

---
+++ Rogue Starfighter VR V1/Assets/Scripts/UI/TextFloatLabelControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextFloatLabelControl : MonoBehaviour
+{
+    [SerializeField] private string prefix = default;
+
+    private TextMeshPro text;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+    }
+
+    public void SetText(float value)
+    {
+        text.text = prefix + value.ToString();
+    }
+}
