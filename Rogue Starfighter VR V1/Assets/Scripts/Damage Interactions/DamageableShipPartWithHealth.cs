dfd487d08da4fa3e28823811631a45cfa83e6f36
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-19 01:16:00 -0700

    Add min max health, add health function to ship health component

--- Rogue Starfighter VR V1/Assets/Scripts/Damage Interactions/DamageableShipPartWithHealth.cs
+++ Rogue Starfighter VR V1/Assets/Scripts/Damage Interactions/DamageableShipPartWithHealth.cs
@@ -3,45 +3,53 @@ using UnityEngine;
 
 public class DamageableShipPartWithHealth : DamageableShipPart
 {
+    [SerializeField] private float minHealth = default;
+    [SerializeField] private float maxHealth = default;
     [SerializeField] private float initialHealth = default;
     private float currentHealth;
     //[SerializeField] private float maxHealth;
 
     public UnityEventFloat OnHealthChange;
+    public UnityEventFloat OnHealthLose;
+    public UnityEventFloat OnHealthGain;
     public UnityEvent OnNoHealth;
 
     [Tooltip("Should I destroy myself when I run out of health?")]
     [SerializeField] private bool destroyOnNoHealth = false;
 
+    // TODO: try compare to minHealth + smallEpsilon if encountering float comparison bug
+    public override bool IsDestroyed => (currentHealth <= minHealth);
+
     private void Start()
     {
-        currentHealth = initialHealth;
-        OnHealthChange.Invoke(currentHealth);
+        SetHealth(initialHealth);
     }
 
-    public override bool IsDestroyed => (currentHealth <= 0.0f);
-
-    public override bool TakeDamage(float damage)
+    public override bool TakeDamage(float amount)
     {
-        if (!IsDestroyed)
+        OnHealthLose.Invoke(amount);
+        return SetHealth(currentHealth - amount);
+    }
+
+    public bool GainHealth(float amount)
+    {
+        OnHealthGain.Invoke(amount);
+        return SetHealth(currentHealth + amount);
+    }
+
+    public bool SetHealth(float newHealth)
+    {
+        newHealth = Mathf.Clamp(newHealth, minHealth, maxHealth);
+        currentHealth = newHealth;
+        OnHealthChange.Invoke(currentHealth);
+        if (IsDestroyed)
         {
-            currentHealth -= damage;
-            OnHealthChange.Invoke(currentHealth);
-            if (IsDestroyed)
+            OnNoHealth.Invoke();
+            if (destroyOnNoHealth)
             {
-                OnNoHealth.Invoke();
-                if (destroyOnNoHealth)
-                {
-                    Destroy(gameObject);
-                }
+                Destroy(gameObject);
             }
         }
         return IsDestroyed;
-    }
-
-    public void SetHealth(float newHealth)
-    {
-        currentHealth = newHealth;
-        OnHealthChange.Invoke(currentHealth);
     }
 }
