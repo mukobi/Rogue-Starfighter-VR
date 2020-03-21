4a2e58f3adda00c2f9692b538fe28af8b8624b2f
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-20 19:13:40 -0700

    Detect when player HMD is outside of desired location

---
+++ Rogue Starfighter VR V1/Assets/Scripts/VR Specific/DetectTargetInRange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectTargetInRange: MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private float range;
+
+    private float sqrRange;
+
+    private void Start()
+    {
+        sqrRange = range * range;
+    }
+
+    public bool TargetIsInRange { 
+        get {
+            return (target.position - transform.position).sqrMagnitude <= sqrRange;
+        }
+    }
+}
