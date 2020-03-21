b6168bb45492a13b6dd2a677169c30f134d86c2e
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-21 01:14:01 -0700

    Make recalibrate UI render on top (TMP.isOverlay)

---
+++ Rogue Starfighter VR V1/Assets/Scripts/UI/TMProOverlaySet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TMProOverlaySet : MonoBehaviour
+{
+    [SerializeField] private bool isOverlay = default;
+
+    void Start()
+    {
+        var textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+        if (textMeshProUGUI != null) textMeshProUGUI.isOverlay = isOverlay;
+        var textMeshPro = GetComponent<TextMeshPro>();
+        if (textMeshPro != null) textMeshPro.isOverlay = isOverlay;
+    }
+}
