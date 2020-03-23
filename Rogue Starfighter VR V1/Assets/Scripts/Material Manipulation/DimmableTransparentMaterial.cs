1a517232ffbfc2aa09f8dd6eb1e1e8c1c805edea
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-23 13:32:07 -0700

    Write DimmableTransparentMaterial

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Material Manipulation/DimmableTransparentMaterial.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimmableTransparentMaterial : DimmableAbstract
+{
+    [SerializeField] private Material material = default;
+    [SerializeField] private string opacityMaterialID = default;
+    public override float DimmableValue01 { set => material.SetFloat(opacityMaterialID, value); }
+}
