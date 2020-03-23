b7f0f16883016bd945195110047ea7204a420644
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-23 13:35:37 -0700

    Write DimmableTextMeshColour

---
+++ Rogue Starfighter VR V1/Assets/Scripts/UI/DimmableTextMeshColour.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DimmableTextMeshColour : DimmableAbstract
+{
+    private TextMeshProUGUI textMesh;
+    private Color initialColor;
+    [SerializeField] private Color fadeColor = default;
+
+    private void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        initialColor = textMesh.color;
+    }
+
+    public override float DimmableValue01 { set => textMesh.color = Color.Lerp(fadeColor, initialColor, value); }
+}
