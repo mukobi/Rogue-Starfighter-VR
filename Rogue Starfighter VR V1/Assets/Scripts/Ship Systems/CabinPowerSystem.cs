9d91d80f73b1842d2d828dd89e862374dc0626d0
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-23 12:20:27 -0700

    Write dimmable abstract pattern and implement for lights (for when cabin power is offline)

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Ship Systems/CabinPowerSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinPowerSystem : ShipSystemAbstract
+{
+    public DimmableAbstract[] dimmables;
+
+    public override string GetShipSystemName => "Cabin Power";
+
+    public override void DisableSystem()
+    {
+        base.DisableSystem();
+        for (int i = 0; i < dimmables.Length; i++)
+        {
+            // TODO: tween with a library instead for flicker effect
+            dimmables[i].DimmableValue01 = 0;
+        }
+    }
+
+    public override void RepairSystem()
+    {
+        base.RepairSystem();
+        for (int i = 0; i < dimmables.Length; i++)
+        {
+            // TODO: tween with a library instead for flicker effect
+            dimmables[i].DimmableValue01 = 1;
+        }
+    }
+}
---
+++ Rogue Starfighter VR V1/Assets/Scripts/Ship Systems/DimmableLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimmableLight : DimmableAbstract
+{
+    private Light lightComponent;
+    private float initialLightIntensity;
+
+    private void Start()
+    {
+        lightComponent = GetComponent<Light>();
+        initialLightIntensity = lightComponent.intensity;
+    }
+
+    public override float DimmableValue01 { set { lightComponent.intensity = value * initialLightIntensity; } }
+}
