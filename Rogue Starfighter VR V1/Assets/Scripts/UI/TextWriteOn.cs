3d0f032bd7610ad65c2308729acb865f1b437446
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-23 00:07:19 -0700

    Write text write on

---
+++ Rogue Starfighter VR V1/Assets/Scripts/UI/TextWriteOn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class TextWriteOn : MonoBehaviour
+{
+    private TextMeshProUGUI textMesh = default;
+
+    [SerializeField] private float characterWriteOnIntervalSeconds = default;
+
+    public UnityEvent OnCharacterWritten;
+
+    private void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        textMesh.text = "";
+    }
+
+    public void WriteOnText(string text)
+    {
+        StartCoroutine(WriteOnTextCoroutine(text));
+    }
+
+    private IEnumerator WriteOnTextCoroutine(string text)
+    {
+        textMesh.text = text;
+        for (int i = 0; i < text.Length; i++)
+        {
+            textMesh.maxVisibleCharacters = i;
+            OnCharacterWritten.Invoke();
+            yield return new WaitForSeconds(characterWriteOnIntervalSeconds);
+        }
+    }
+}
