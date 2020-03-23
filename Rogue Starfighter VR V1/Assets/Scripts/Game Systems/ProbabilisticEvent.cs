08ef3b678908172d35c53923b257a2e18c7cf731
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-22 23:30:48 -0700

    Write random chance script that probabilistically invokes an event when called

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Game Systems/ProbabilisticEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class ProbabilisticEvent : MonoBehaviour
+{
+    [SerializeField] [Range(0, 1)] private float probability = default;
+
+    public UnityEvent OnLuckyRoll;
+
+    public void AttemptProbabilisticEvent()
+    {
+        if (Random.value <= probability)
+        {
+            OnLuckyRoll.Invoke();
+        }
+    }
+}
