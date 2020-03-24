ff947fedce5d4d7d1972f26b17194c8e8ed96b63
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-24 13:17:32 -0700

    Create random one of many event triggerer that randomly calls one of a list of events (e.g. for random positioned spark VFX or random TIE spawns)

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Events/BaseEvent.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public abstract class BaseEvent : MonoBehaviour
+{
+    public UnityEvent OnEvent;
+}
---
+++ Rogue Starfighter VR V1/Assets/Scripts/Events/RandomOneOfManyEvents.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomOneOfManyEvents : MonoBehaviour
+{
+    [SerializeField] private BaseEvent[] events = default;
+
+    public void InvokeSingleRandomEvent()
+    {
+        events[Random.Range(0, events.Length)].OnEvent.Invoke();
+    }
+}
