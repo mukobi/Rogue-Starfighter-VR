3200426d274cd72884cbb6571e8946626fd1b38a
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-23 18:09:01 -0700

    Write delayed event script

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Events/Delayed Event.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+using System.Collections;
+
+public class DelayedEvent : MonoBehaviour
+{
+    [SerializeField] private float delaySeconds = default;
+
+    [SerializeField] UnityEvent OnEvent;
+
+    public void TriggerEventAfterDelay()
+    {
+        StartCoroutine(DelayedEventCoroutine());
+    }
+
+    private IEnumerator DelayedEventCoroutine()
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        OnEvent.Invoke();
+    }
+}
