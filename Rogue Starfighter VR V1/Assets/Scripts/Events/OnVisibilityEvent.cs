dd53f258098c47ba346c9ca1428a1b8647d8442f
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-23 17:59:38 -0700

    Write OnVisibilityEvent script

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Events/OnVisibilityEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class OnVisibilityEvent : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnVisible = default;
+    [SerializeField] private UnityEvent OnInvisible = default;
+
+    private void OnBecameVisible()
+    {
+        OnVisible.Invoke();
+    }
+
+    private void OnBecameInvisible()
+    {
+        OnInvisible.Invoke();
+    }
+}
