a6e89eec590bb12fd71a10f1f56c34cf5050808d
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-03-25 11:19:14 -0700

    work on hyperdrive switch functiuonality

---
+++ Rogue Starfighter VR V1/Assets/Scripts/Ship Controls/HyperdriveSwitchController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class HyperdriveSwitchController : MonoBehaviour
+{
+    [Header("Dependencies")]
+    public CircularDrive switchCircularDrive;
+
+    [Header("Config")]
+    [SerializeField] private Vector2 lockedInitialMinMaxAngles = default;
+    [SerializeField] private Vector2 lockedForwardMinMaxAngles = default;
+    [SerializeField] private Vector2 freeMinMaxAngles = default;
+
+
+    public bool SwitchHasBeenThrown { get; set; } = false;
+
+    private bool isLocked = true;
+
+    public async Task RequireSwitchThrown(CancellationToken ct)
+    {
+        FreeSwitch();
+        SwitchHasBeenThrown = false;
+        while (!ct.IsCancellationRequested && !SwitchHasBeenThrown)
+        {
+            await Task.Delay(50);
+        }
+        SwitchHasBeenThrown = false;
+    }
+
+    public void RotateSwitchToPosition(float rotation)
+    {
+
+    }
+    
+    private void LockSwitchInInitialPosition()
+    {
+        isLocked = true;
+        switchCircularDrive.minAngle = lockedInitialMinMaxAngles.x;
+        switchCircularDrive.maxAngle = lockedInitialMinMaxAngles.y;
+        RotateSwitchToPosition(lockedInitialMinMaxAngles.x);
+    }
+
+    private void LockSwitchInForwardPosition()
+    {
+        isLocked = true;
+        switchCircularDrive.minAngle = lockedForwardMinMaxAngles.x;
+        switchCircularDrive.maxAngle = lockedForwardMinMaxAngles.y;
+        RotateSwitchToPosition(lockedForwardMinMaxAngles.x);
+    }
+
+    private void FreeSwitch()
+    {
+        isLocked = true;
+        switchCircularDrive.minAngle = freeMinMaxAngles.x;
+        switchCircularDrive.maxAngle = freeMinMaxAngles.y;
+    }
+}
